addb24b3f25f116a467627c0072c5977845ecc67
Kiki Dawson <kikiadawson@gmail.com>
2024-03-18 10:53:06 +1300

    add invoke url to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "execution_arn" {
   description = "The execution ARN to be used for lambda permission."
   value       = aws_api_gateway_rest_api.this.execution_arn
 }
+
+output "invoke_url" {
+  description = "The URL used to invoke the API."
+  value       = aws_api_gateway_stage.this.invoke_url
+}
